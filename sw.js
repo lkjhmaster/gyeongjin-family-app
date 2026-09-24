945b20bd052bfe7eef72c18a858df8d725bdc546
lkjhmaster <lkjh0827@gmail.com>
2026-09-25 02:59:13 +0900

    Refresh PWA cache for final icon design

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='gyeongjin-family-app-v2026.09.25-pwa3-cute-icon';
+const CACHE_NAME='gyeongjin-family-app-v2026.09.25-pwa4-final-icon';
 const APP_SHELL=['./','./index.html','./manifest.json','./icon-192.svg','./icon-512.svg','./sw.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
